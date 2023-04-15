80e45e85b0c951ab9c5b1b946cc4e4124eedd808
Leonardo Berardino <leonardo@gscloudsolutions.com>
2023-04-15 09:24:16 -0700

    Update eslint version and configuration

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["src/**/*.js"],
+    ignores: ["node_modules/**"],
+    languageOptions: {
+      ecmaVersion: 2022,
+    },
+    rules: {
+      semi: "error",
+      "prefer-const": "error",
+    },
+  },
+];
